b93c957b67a447ba41110d02950ed293548e20dd
mic-se <none>
2019-02-14 14:52:02 +0100

    fix

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -6,11 +6,20 @@ const router = Router();
 router.get('/', (req, res) => {
   todosModel.find( (err, todos) => {
     if (err) {
-      console.log(err);
-      return res.status(500).send('Internal error');
+      return res.status(400).json(err);
     }
 
-    return res.json(todos);
+    return res.status(200).json(todos);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  todosModel.findById(req.params.id, (err, todos) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+
+    return res.status(200).json(todos);
   });
 });
 
@@ -18,39 +27,34 @@ router.post('/', (req, res) => {
   const todo = new todosModel(req.body);
   todo.save()
     .then(todo => {
-      return res.status(201).json({'todo': 'item added successfully'});
+      return res.status(201).json(todo);
     })
     .catch(err => {
-      console.log(err);
-      return res.status(400).send("Wrong data");
+      return res.status(400).json(err);
     });
 });
 
-router.put('/:name', (req, res, next) => {
-  todosModel.findOneAndUpdate({name: req.params.name}, req.body, {new: true}, (err, todo) => {
+router.put('/:id', (req, res, next) => {
+  todosModel.findByIdAndUpdate(req.params.id, req.body, {runValidators: true, new: true}, (err, todo) => {
     if (err) {
-      console.log(err);
-      return res.status(500).send('Internal error');
+      return res.status(400).json(err);
     }
 
-    return res.status(200).send(todo);
+    return res.status(200).json(todo);
   });
-
-  return res.status(204);
 });
 
-router.delete('/:name', (req, res) => {
-  todosModel.findOneAndDelete({name: req.params.name}, (err, todo) => {
+router.delete('/:id', (req, res) => {
+  todosModel.findByIdAndDelete(req.params.id, (err, todo) => {
     if (err) {
-      console.log(err);
-      return res.status(500).send('Internal error');
+      return res.status(400).json(err);
     }
 
     if (!todo) {
-      return res.status(404).send("Not found");
+      return res.status(404).json("Not found");
     }
 
-    return res.status(204);
+    return res.status(204).json("Deleted");
   });
 });
 
